cc4ab1ccc3b0a8a171910ca0f586f7385dc859fc
rcabanjin <113873622+rcabanjin@users.noreply.github.com>
2023-02-05 21:25:37 -0800

    update chamber

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,21 @@
+let date = new Date();
+let currentDate = date.toLocaleDateString('en-UK', { weekday:"long", day:"numeric", month:"long", year:"numeric"})
+document.getElementById("currentDate").textContent = currentDate;
+let copyrightYear = date.getFullYear();
+document.getElementById("currentYear").textContent = copyrightYear;
+const lastModified = document.lastModified;
+document.getElementById("lastupdated").textContent = lastModified;
+let currentDay = date.getDay();
+if (currentDate !== 1 || currentDate !== 2) {
+    document.querySelector(".ad").style.display = "none";}
+
+
+function toggleMenu() {
+    document.getElementById("primary-nav").classList.toggle("open");
+    document.getElementById("hamburger-button").classList.toggle("open");
+    }
+    
+    const x = document.getElementById("hamburger-button");
+    
+    x.onclick = toggleMenu;
+    

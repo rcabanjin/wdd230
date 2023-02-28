d66d68a4e66454e18ae60832a5e7709a18106099
rcabanjin <113873622+rcabanjin@users.noreply.github.com>
2023-02-27 22:45:53 -0800

    discover week07

---
+++ chamber/scripts/page-visit.js
@@ -0,0 +1,16 @@
+const visitsDisplay = document.querySelector("#visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls")); // Using the Number() function ensures that if the storage item does not exist, it will be converted into a zero (0) which helps on the if block condition.
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);

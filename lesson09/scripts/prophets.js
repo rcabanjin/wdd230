d8ee03aa310bcd3574470f93d678d7c32ee40d40
rcabanjin <113873622+rcabanjin@users.noreply.github.com>
2023-03-02 22:45:28 -0800

    Week09activity

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -3,35 +3,47 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);
+    console.table(data.prophets);
     displayProphets(data.prophets);
 }
 
 getProphetData();
 
+function prophetAge(birthdate, deathdate) {
+    const birth = new Date(birthdate);
+    const death = deathdate ? new Date(deathdate) : new Date();
+    const ageDiff = death - birth;
+    const ageDate = new Date(ageDiff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+ 
 const displayProphets = (prophets) => {
-    const cards = document.querySelector('div.cards');  // select the output container element
+    const cards = document.querySelector('div.cards'); // select the output container element
 
     prophets.forEach((prophet) => {
         //Create elements to add to the div.cards element
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
+        let h3 = document.createElement('h3');
         let portrait = document.createElement('img');
 
         //Build the h2 content out to show the prophet's full name - finish the template string
-        h2.textContent = '${prophet.name} _________________';
+        h2.textContent = `${prophet.name} ${prophet.lastname}`;
+        h3.innerHTML = `Birth: ${prophet.birthdate}<br>Birth: ${prophet.birthplace}<br>Children: ${prophet.numofchildren}<br>Age: ${prophetAge(prophet.birthdate, prophet.death)}<br>Years served: ${prophet.length}`;
 
         //Build the image portrait by setting all the relevant attributes
         portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', 'Portrait of ${prophet.name} __________________');
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}${prophet.order === 1 ? "st" : prophet.order === 2 ? "nd" : "th"} Latter-day President`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
 
         // Append the section(card_ with the created elements
-        card_.appendChild(h2);
+       
+        card.appendChild(h2);
+        card.appendChild(h3);
         card.appendChild(portrait);
 
         cards.appendChild(card);
-    } // end of forEach loop
-}//endof function expression
+    }); // end of forEach loop
+}//endof function expression 
